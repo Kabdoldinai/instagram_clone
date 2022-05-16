8d43280b812a7c32d173b3a193eb96f4166c7252
kabdoldinai <kabdoldinai@gmail.com>
2022-05-16 07:57:28 +0600

    adde right side menu and profile img

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import { SearchIcon } from '@heroicons/react/outline';
+import { SearchIcon, PlusCircleIcon } from '@heroicons/react/outline';
+import { HomeIcon } from '@heroicons/react/solid';
 
 export default function Header(){
     return (
-        <div className="flex items-center justify-between max-w-6xl">
+        <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
             {/*Left*/}
             <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid ">
                 <Image src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo-2010-2013.png"
@@ -26,7 +27,11 @@ export default function Header(){
                 <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"/>
             </div>
             {/*Right*/}
-            <h1>Right side</h1>
+            <div className="flex space-x-4 items-center">
+                <HomeIcon className="hidden md:inline-flex h-5 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"/>
+                <PlusCircleIcon className="h-5 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"/>
+                <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEMgk9uzDHRyg/profile-displayphoto-shrink_800_800/0/1602739338338?e=1658361600&v=beta&t=TmeVF-66cQzsPz383jN1pUA8a3wegtg8Qh0J1ggmqmc" className='h-10 rounded-full cursor-pointer' alt='user-img'/>
+            </div>
         </div>
     )
 }
