d5477c6997993c4382a67554ad6c9baf3e452ea5
kabdoldinai <kabdoldinai@gmail.com>
2022-05-16 07:34:11 +0600

    added search in header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Image from "next/image"
+import Image from "next/image";
+import { SearchIcon } from '@heroicons/react/outline';
 
 export default function Header(){
-    return <div>
-        {/*Left*/}
+    return (
         <div className="flex items-center justify-between max-w-6xl">
+            {/*Left*/}
             <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid ">
                 <Image src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo-2010-2013.png"
                        layout='fill'
@@ -16,9 +17,16 @@ export default function Header(){
                        className="object-contain"/>
             </div>
 
+            {/*Middle*/}
+
+            <div className="relative mt-1">
+                <div className="absolute top-2 left-2">
+                    <SearchIcon className="h-5 text-gray-500"/>
+                </div>
+                <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"/>
+            </div>
+            {/*Right*/}
             <h1>Right side</h1>
         </div>
-        {/*Middle*/}
-        {/*Right*/}
-    </div>
+    )
 }
