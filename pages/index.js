534651aa72f216df0475f7f1d486e9e55dc09a34
kabdoldinai <kabdoldinai@gmail.com>
2022-05-23 20:15:19 +0600

    install recoil and add atom and upload modal component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from "../components/Header";
 import Feed from "../components/Feed";
+import UploadModal from "../components/UploadModal";
 
 
 export default function Home() {
@@ -16,6 +17,8 @@ export default function Home() {
         {/*Feed*/}
         <Feed/>
         {/*Modal*/}
+
+        <UploadModal/>
     </div>
   )
 }
