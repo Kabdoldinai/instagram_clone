2bb3caaecd8be85abdd138d02f4176a2d6c79758
kabdoldinai <kabdoldinai@gmail.com>
2022-05-23 19:55:18 +0600

    modified miniprofile and story component to conclude the session and made responsive

--- components/Story.js
+++ components/Story.js
@@ -1,8 +1,9 @@
-import React from "react";
+import {PlusIcon} from "@heroicons/react/solid";
 
-export default function Story({img, username}) {
-    return <div>
-        <img className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition:transition-transform duration-200 ease-out"  src={img} alt={username}/>
+export default function Story({img, username, isUser}) {
+    return <div className="relative group cursor-pointer">
+        <img className="h-14 rounded-full p-[1.5px] border-red-500 border-2  group-hover:scale-110 transition:transition-transform duration-200 ease-out"  src={img} alt={username}/>
+        {isUser && <PlusIcon className="h-6 absolute top-4 left-4 text-white"/>}
         <p className="text-xs w-14 truncate">{username}</p>
     </div>;
 }
